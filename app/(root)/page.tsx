6d846832c423b240321b5bf0a56d798fe8449f83
oreolion <remyoreo11@gmail.com>
2024-06-30 23:53:43 -0700

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section className="">
+        <Navbar></Navbar>
+      </section>
+    </main>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "RA | Blog and read about your Interest",
+  description: "AI Powered Blogging App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
