2c9537b11171d2e0e9f269bc3b252227192be54f
oreolion <remyoreo11@gmail.com>
2024-07-01 04:02:45 -0700

    minor fix;

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,7 +8,7 @@ import ThirdSection from "@/components/ThirdSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section className="">
+      <section>
         <Navbar></Navbar>
         <Hero></Hero>
         <About></About>
