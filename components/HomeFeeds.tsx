e4b4794ade53e70212375fea02b362e08e8e4544
oreolion <remyoreo11@gmail.com>
2024-07-05 02:47:54 -0700

    update home feeds component;

--- components/HomeFeeds.tsx
+++ components/HomeFeeds.tsx
@@ -42,7 +42,7 @@ const HomeFeeds = () => {
           >
             <div className={styles.user__profile}>
               <div className={styles.user__image}>
-                <span className="" v-if="!post.userPhotoURL">
+                <span className={styles.span} v-if="!post.userPhotoURL">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 448 512"
@@ -88,7 +88,7 @@ const HomeFeeds = () => {
                   >
                     <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
                   </svg>
-                  <span>4 views</span>
+                  <span className={styles.span}>4 views</span>
                 </div>
               </div>
               <div className={styles.right}>
