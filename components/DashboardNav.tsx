74e05bc5d11ced7493a008e4fc1996f5d00c31c3
oreolion <remyoreo11@gmail.com>
2024-07-12 03:36:11 -0700

    add searchbar component;

--- components/DashboardNav.tsx
+++ components/DashboardNav.tsx
@@ -74,7 +74,6 @@ const DashboardNav = () => {
             </Link>
           </li>
 
-         
           <li className={styles.li}>
             <Link className={styles.link} href="/dashboard/postanalytics">
               <svg
@@ -87,11 +86,22 @@ const DashboardNav = () => {
               Analytics
             </Link>
           </li>
-          <h5 className={styles.h5}>Trending Tags</h5>
+            <span className={styles.spanheader}>
+          <h5 className={styles.h5}>
+            Trending Tags
+              
+          </h5>
+          <svg className={styles.svg} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+              </svg>
+          </span>
           {postCategories.map((category) => {
             return (
               <li className={styles.li} key={category}>
-                <Link href={`/filter-posts/${category}`} className={styles.link}>
+                <Link
+                  href={`/filter-posts/${category}`}
+                  className={styles.link}
+                >
                   {category}
                 </Link>
               </li>
@@ -101,17 +111,17 @@ const DashboardNav = () => {
           <h5 className={styles.h5}>Personal</h5>
 
           <li className={styles.li}>
-                <Link className={styles.link} href={`/profile/${user?.id}`}>
-                  <svg
-                    className={styles.svg}
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 448 512"
-                  >
-                    <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                  </svg>
-                  <p className={styles.p}>My Profile</p>
-                </Link>
-              </li>
+            <Link className={styles.link} href={`/profile/${user?.id}`}>
+              <svg
+                className={styles.svg}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+              </svg>
+              <p className={styles.p}>My Profile</p>
+            </Link>
+          </li>
           <li className={styles.li}>
             <Link className={styles.link} href="/dashboard/nocontent">
               <svg
--- components/MobileDashBoardNav.tsx
+++ components/MobileDashBoardNav.tsx
@@ -7,6 +7,7 @@ import React, { useState } from "react";
 import styles from "@/styles/mobiledashboardNav.module.css";
 import { useRouter } from "next/navigation";
 import { postCategory } from "@/types";
+import SearchBar from "./SearchBar";
 
 const MobileDashBoardNav = () => {
   const [toggle, setToggle] = useState<boolean>(false);
@@ -86,7 +87,6 @@ const MobileDashBoardNav = () => {
                   Bookmarks
                 </Link>
               </li>
-
               <li className={styles.li}>
                 <Link className={styles.link} href="/dashboard/postanalytics">
                   <svg
@@ -99,7 +99,16 @@ const MobileDashBoardNav = () => {
                   Analytics
                 </Link>
               </li>
-              <h5 className={styles.h5}>Trending Tags</h5>
+              <span className={styles.spanheader}>
+                <h5 className={styles.h5}>Trending Tags</h5>
+                <svg
+                  className={styles.svg}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+                </svg>
+              </span>{" "}
               {postCategories.map((category) => {
                 return (
                   <li className={styles.li} key={category}>
@@ -112,9 +121,7 @@ const MobileDashBoardNav = () => {
                   </li>
                 );
               })}
-
               <h5 className={styles.h5}>Personal</h5>
-
               <li className={styles.li}>
                 <Link className={styles.link} href={`/profile/${user?.id}`}>
                   <svg
@@ -127,7 +134,6 @@ const MobileDashBoardNav = () => {
                   <p className={styles.p}>My Profile</p>
                 </Link>
               </li>
-
               <li className={styles.li}>
                 <Link className={styles.link} href="/dashboard/nocontent">
                   <svg
@@ -212,24 +218,7 @@ const MobileDashBoardNav = () => {
               </svg>
             </div>
           </nav>
-          <div className={styles.search_form}>
-            <label>
-              <div className={styles.search}>
-                <svg
-                  className={styles.svg}
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                >
-                  <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
-                </svg>
-              </div>
-            </label>
-            <input
-              className={styles.input}
-              type="search"
-              placeholder="search and Read Along..."
-            />
-          </div>
+        <SearchBar></SearchBar>
           <svg
             className={styles.caret}
             xmlns="http://www.w3.org/2000/svg"
