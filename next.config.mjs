3eb71b928c097db22abe5da25c4095fe446865df
oreolion <remyoreo11@gmail.com>
2024-07-31 03:12:42 -0700

    minor fix to next config;

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,6 @@ const nextConfig = {
     },
     reactStrictMode: true,
     swcMinify: true,
-    async rewrites() {
-        return [
-          {
-            source: '/:path*',
-            destination: '/:path*',
-          },
-        ]
-      },
     images: {
       remotePatterns: [
         {
