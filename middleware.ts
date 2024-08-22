8a8f9ca7bc047dfebf0ae66c2bdb1a93f87a5184
oreolion <remyoreo11@gmail.com>
2024-08-22 04:39:10 -0700

    fix;

--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/']);
-const isHomeRoute = createRouteMatcher(['/']);
-const isDashboardRoute = createRouteMatcher(['/dashboard(.*)']);
 
 export default clerkMiddleware((auth, req) => {
-  const { userId } = auth();
-  const { pathname } = new URL(req.url);
+    if (!isPublicRoute(req))  auth().protect();
+    // Restrict admin route to users with specific role
+  //   if (isAdminRoute(req)) auth().protect({ role: 'org:admin' });
 
-  if (isPublicRoute(req)) {
-    return NextResponse.next();
-  }
-
-  if (!isPublicRoute(req))  auth().protect();
-
-  if (isHomeRoute(req)) {
-    if (userId) {
-    //   return NextResponse.redirect(new URL('/dashboard', req.url));
-    return NextResponse.next();
-    }
-  }
-
-  if (isDashboardRoute(req)) {
-    if (!userId) {
-      return NextResponse.redirect(new URL('/sign-in', req.url));
-    }
-    return NextResponse.next();
-  }
-
-  // For all other routes, protect access
-  if (!userId) {
-    return NextResponse.redirect(new URL('/sign-in', req.url));
-  }
-
-
-  return NextResponse.next();
 });
 
 export const config = {
