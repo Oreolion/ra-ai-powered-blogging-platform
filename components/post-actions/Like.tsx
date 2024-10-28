484da4211b23f488f2b932a19c2fbbdbb387e245
oreolion <remyoreo11@gmail.com>
2024-10-28 10:40:07 +0300

    update mobile responsiveness;

--- components/post-actions/Like.tsx
+++ components/post-actions/Like.tsx
@@ -72,7 +72,7 @@ const Like = ({ likes, postId }: { likes: number; postId: string }) => {
           className="opacity-50 hover:opacity-100 cursor-pointer"
         />
       )}
-      <span className="text-xl">{formattedNumber}</span>
+      <span className="text-xl ml-2">{formattedNumber}</span>
     </div>
   );
 };
