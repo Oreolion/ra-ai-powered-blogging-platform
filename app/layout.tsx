9b7274852ef5f90a127d5a7efeca35852999d0da
REMY ADEDEJI  #RA <remyoreo11@gmail.com>
2025-03-28 09:08:09 -0700

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;cre
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
