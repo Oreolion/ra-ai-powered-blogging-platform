0acad9f5a14237d7508000b6f8e12229bf6824a9
REMY ADEDEJI  #RA <remyoreo11@gmail.com>
2025-08-30 07:24:21 -0700

    minor fix;

--- components/MobileDashBoardNav.tsx
+++ components/MobileDashBoardNav.tsx
@@ -226,20 +226,21 @@ const MobileDashBoardNav = () => {
                     <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
                   </svg>
                 )}
-
               </div>
-                <svg
-                  className="w-8 h-8 fill-current text-slate-400 hover:text-white cursor-pointer transition-colors md:mr-8"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512"
-                  onClick={toggleDropDown}
-                >
-                  <title>Notifications</title>
-                  <path d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416H416c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z" />
-                </svg>
+              <svg
+                className="w-8 h-8 fill-current text-slate-400 hover:text-white cursor-pointer transition-colors md:mr-8"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                onClick={toggleDropDown}
+              >
+                <title>Notifications</title>
+                <path d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416H416c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z" />
+              </svg>
 
               <div className="w-10 h-10 items-center md:mt-8 md:w-16 md:h-16">
-                <UserButton />
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
               </div>
             </div>
 
